fdbbe9dfb8c21256ce2d4e0423ba21b55877c97d
Nilavra Ghosh <nilavra.ghosh@students.iiit.ac.in>
2025-07-12 23:31:32 +0530

    helper functions

--- BigInt.cpp
+++ BigInt.cpp
@@ -7,4 +7,26 @@ class BigInt{
 public:
 
     //Constructors:
+    BigInt(unsigned long long n = 0);
+    BigInt(string &);
+    BigInt(const char *);
+    BigInt(BigInt &);
+
+    //Helper Functions:
+    friend void divide_by_2(BigInt &a);
+    friend bool Null(const BigInt &);
+    friend int Length(const BigInt &);
+    int operator[](const int)const;
+
+               /* * * * Operator Overloading * * * */
+
+    //Direct assignment
+    BigInt &operator=(const BigInt &);
+
+    //Post/Pre - Incrementation
+    BigInt &operator++();
+    BigInt operator++(int temp);
+    BigInt &operator--();
+    BigInt operator--(int temp);
+
 }
